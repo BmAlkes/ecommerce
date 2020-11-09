71178484747febf65a539871c22a07af5d447501
unknown <bmalkes@gmail.com>
2020-11-09 18:00:18 +0200

    push1

---
+++ produtos.js
@@ -0,0 +1,28 @@
+let produtos = [
+  {
+    id: 1,
+    name: "Red Printed T-Shirt",
+    img: "./assets/images/product-1.jpg",
+    price: 30.00,
+    sizes: ["small", "medium", "large"," XLarge"],
+    description:
+      "Give your summer Wardrobe a style upgrade with the HrX Men's Active T-Shirt. Team it with a pair of shorts for your morning workout or a denims for a evening out with the guys.",
+  },
+  {
+    id: 2,
+    name: "Black Shoes HRX",
+    img: "./assets/images/product-2.jpg",
+    price: 15.00,
+    sizes: ["40", "42", "44"," 46"],
+    description:"holiday for your feet, ultra comfortable with extra shock absorber.",
+  },
+  {
+    id: 3,
+    name: "Fitness Panty",
+    img: "./assets/images/product-2.jpg",
+    price: 15.00,
+    sizes: ["40", "42", "44"," 46"],
+    description:"holiday for your feet, ultra comfortable with extra shock absorber.",
+  },
+  
+];
---
+++ script.js
@@ -0,0 +1,45 @@
+let menuItems = document.getElementById('menuItens')
+
+
+
+let icon = document.querySelector('.menu-icon')
+
+icon.addEventListener('click', ()=>{
+menuItems.classList.toggle('active')
+console.log('cliccou')
+})
+
+
+let bigImg = document.querySelector("#product-img")
+
+let smallImg = document.querySelectorAll(".small-img-col img")
+
+
+
+
+smallImg.forEach((img)=>{
+    img.addEventListener("click",()=>{
+        console.log(bigImg)
+        bigImg.src = img.getAttribute('src')
+    })
+});
+
+// Form 
+
+let loginForm = document.getElementById('loginForm')
+let RegForm = document.getElementById('regForm')
+
+let Indicator = document.getElementById("Indicator")
+
+
+function register(){
+    RegForm.style.transform ="translateX(0px)";
+    loginForm.style.transform = "translateX(0px)"
+    Indicator.style.transform ="translateX(100px)"
+}
+
+function login(){
+    RegForm.style.transform ="translateX(300px)";
+    loginForm.style.transform = "translateX(300px)"
+    Indicator.style.transform ="translateX(0px)"
+}
